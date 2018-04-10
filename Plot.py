7a2472238877b34392924b1851047d17d56ac8cb
yidwa <yidan12.wang@gmail.com>
2018-04-10 22:36:17 +1000

    first commit

---
+++ Plot.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import re
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+xs = []
+ys = []
+zs = []
+groups = []
+
+
+def ini_Cor():
+    with open("coordinates","r") as ins:
+        for line in ins:
+            # print line
+           fline = line.split()
+
+           groups.append(fline[0])
+           xs.append(float(fline[1]))
+           ys.append(float(fline[2]))
+           if(len(fline)>3):
+              zs.append(float(fline[3]))
+           else:
+              zs.append(0)
+
+ini_Cor()
+#colors= ("red", "green", "blue")
+
+print groups
+print xs
+print ys
+print zs
+for  group, x, y, z in zip( groups, xs, ys, zs):
+   # label = '%s (%d, %d, %d)' % (group, x,y,z)
+   label = group
+   ax.scatter(xs, ys, zs)
+   ax.text(x,y,z,label)
+
+#
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Availability')
+# ax.set_xlim3d(0,25)
+# ax.set_ylim3d(0,25)
+ax.set_zlim3d(0,13)
+
+# ax.scatter(xs,ys,zs)
+
+
+# for xs, ys, zs, color, group in zip(xs, ys, zs, colors, groups):
+#     ax.scatter(xs,ys,zs, alpha=0.8, c = color, edgecolors='none', s=30, label = group)
+
+plt.legend(loc=2)
+plt.show()
